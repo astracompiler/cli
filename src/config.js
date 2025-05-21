3087c3203b956c8fbf1dc36d83d81483bc64b54e
QwertyCode <qwerty@qwerty.daxel.pl>
2025-05-21 16:49:56 +0200

    fix: update exe property types in config.js for better type safety

--- src/config.js
+++ src/config.js
@@ -5,8 +5,10 @@
  * @property {Object} [esbuild] - esbuild options (see https://esbuild.github.io/api/)
  * @property {string[]} [assets] - Assets to include in the blob
  * @property {boolean} [modifyMetadata] - Defaults to true. If false, the metadata (icon, copyright) will not be modified.
- * @property {{ icon: string, companyName: string, fileDescription: string, productName: string, fileVersion: string, productVersion: string, copyright: string }} [exe] - Application metadata (like icon, name, description etc.)
+ * @property {{ icon: string | undefined, companyName: string | undefined, fileDescription: string | undefined, productName: string | undefined, fileVersion: string | undefined, productVersion: string | undefined, copyright: string | undefined }} [exe] - Application metadata (like icon, name, description etc.)
  */
+
+/** @type {Config} */
 export default {
   outFile: 'dist/app.exe',
 
@@ -17,11 +19,12 @@ export default {
   modifyMetadata: true, // modify metadata of the executable (useful if you have macOS and don't have wine installed)
 
   exe: {
-    companyName: 'Your Company',             // your company name
-    productName: 'Your App',                 // name of your app
-    fileDescription: 'Your App Description', // description of your app
-    productVersion: '1.0.0',                 // version of your app
-    fileVersion: '1.0.0.0',                  // file version
-    icon: 'path/to/icon.ico',                // path to the app icon (optional)
+    companyName: 'Your Company',
+    productName: 'Your App',
+    fileDescription: 'Your App Description',
+    productVersion: '1.0.0',
+    fileVersion: '1.0.0.0',
+    icon: 'path/to/icon.ico',
+    copyright: 'Copyright © 2023 Your Company',
   }
 }
