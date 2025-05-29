926aca51a3c19b642b7889aa2fb4de89ec7e9290
QwertyCode <qwerty@qwerty.daxel.pl>
2025-05-29 22:11:35 +0200

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,12 +3,12 @@ export default defineConfig({
   test: {
     coverage: {
       provider: 'v8',
+      include: [
+        "src",
+      ],
       exclude: [
-        "src/config.js",
-        "compiletest.js",
-        "astra.config.js",
-        "minify.tool.js",
-        "dist",
+        "src/config.js", // This is a template file
+        "src/astra.ts" // This is the main entry file (in CLI tests this file is not included)
       ]
     },
   },
